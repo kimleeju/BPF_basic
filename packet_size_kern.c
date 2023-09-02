b823c0072d633419e908b0c4d95e99a1cd268ee9
kimleeju <kimleju1408@gmail.com>
2023-09-02 21:32:01 +0900

    modified error

--- packet_size_kern.c
+++ packet_size_kern.c
@@ -11,7 +11,7 @@ struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __type(key, __u32);
     __type(value, __u64);
-    __uint(max_entries, 2);
+    __uint(max_entries, 3);
 } packet_size_map SEC(".maps"); // 수정할 부분
 
 
@@ -30,32 +30,38 @@ static inline int bpf_memcmp(const void *s1, const void *s2, unsigned int n) {
 
 SEC("prog")
 int drop_all(struct __sk_buff *skb) {
-    __u32 size_key = 0; // Since max_entries is 1, we'll just use 0 as the key
-    __u32 offset_key = 1; // Since max_entries is 1, we'll just use 0 as the key
+    __u32 size = 0; // Since max_entries is 1, we'll just use 0 as the key
+    __u32 value_offset = 1; // Since max_entries is 1, we'll just use 0 as the key
    __u64 packet_size = skb->len;
 
     char delimiter[2];
     char redis_cmd[3];
     struct tcphdr tcp_header;
-    if (bpf_skb_load_bytes(skb, ETH_HLEN + sizeof(struct iphdr), &tcp_header, sizeof(tcp_header)) != 0)
-        return -1; // Drop packet
+    bpf_printk("333333333333333\n");
+    if (bpf_skb_load_bytes(skb, ETH_HLEN + sizeof(struct iphdr), &tcp_header, sizeof(tcp_header)) != 0){
+        packet_size = 0;
+        bpf_map_update_elem(&packet_size_map, &size, &packet_size, BPF_ANY);
+        return -1;
+    }
    
-
     //__u32 tcp_hdr_len = tcp_header.doff*4;
     __u32 tcp_hdr_len = tcp_header.doff*2;
 
-    if(bpf_skb_load_bytes(skb, ETH_HLEN+sizeof(struct iphdr)+tcp_hdr_len, &redis_cmd,sizeof(redis_cmd)) !=0 )
-         return -1;
+    if(bpf_skb_load_bytes(skb, ETH_HLEN+sizeof(struct iphdr)+tcp_hdr_len, &redis_cmd,sizeof(redis_cmd)) !=0 ){
+        packet_size = 0;
+        bpf_map_update_elem(&packet_size_map, &size, &packet_size, BPF_ANY);
+        return -1;
+    }
 
-    if(bpf_memcmp(redis_cmd,"set",3)!=0 && bpf_memcmp(redis_cmd,"SET",3)!=0)
-         return -1;
-
+    if(bpf_memcmp(redis_cmd,"set",3)!=0 && bpf_memcmp(redis_cmd,"SET",3)!=0){
+        packet_size = 0;
+        bpf_map_update_elem(&packet_size_map, &size, &packet_size, BPF_ANY);
+        return -1;
+    }
     __u64 offset = ETH_HLEN+sizeof(struct iphdr)+tcp_hdr_len+3; // set(3) \r\n(2) $3\r\n(4) key\r\n(5)
      
      /* Now we need to find the next \r\n */
      
-
-
     for (int j = 0; j < 4; ++j){
         for(int i=0; i<512; ++i){ // Assuming a maximum of 512 bytes to search
             if(bpf_skb_load_bytes(skb,offset+i, &delimiter,sizeof(delimiter))!=0)
@@ -69,8 +75,9 @@ int drop_all(struct __sk_buff *skb) {
     }
     packet_size -= offset + 2; // 2(\r\n)
     offset -= (ETH_HLEN+sizeof(struct iphdr)-2);
-    bpf_map_update_elem(&packet_size_map, &size_key, &packet_size, BPF_ANY);
-    bpf_map_update_elem(&packet_size_map, &offset_key, &offset, BPF_ANY);
+    
+    bpf_map_update_elem(&packet_size_map, &size, &packet_size, BPF_ANY);
+    bpf_map_update_elem(&packet_size_map, &value_offset, &offset, BPF_ANY);
     return -1;
 
 }
